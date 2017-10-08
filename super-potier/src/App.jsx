be9782681d0d9c91e7092c6d9607d8550e76f16f
jordanbenzacken <jbzkjbzk@gmail.com>
2017-10-08 12:51:41 +0200

    les routes

--- super-potier/src/App.jsx
+++ super-potier/src/App.jsx
@@ -1,21 +1,31 @@
 import React, {Component} from 'react';
 import logo from './assets/polygon-harry.jpg';
 import SearchView from './views/search-view';
+import Detail from './views/detail';
+import NoMatch from './views/no-match';
+
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="app-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to Super Potier Store</h1>
-        </header>
-        <body className="App-body">
-          <SearchView/>
-        </body>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="app-logo" alt="logo"/>
+            <h1 className="App-title">Welcome to Super Potier Store</h1>
+          </header>
+          <body className="App-body">
+            <Switch>
+              <Route exact path="/" component={SearchView}/>
+              <Route path="/detail" component={Detail}/>
+              <Route component={NoMatch}/>
+            </Switch>
+          </body>
+        </div>
+      </Router>
     );
   }
 }
---
+++ super-potier/src/views/no-match.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class NoMatch extends React.Component {
+    render() {
+        return (
+            <div className='shake'>Crac badaboum</div>
+        )
+    }
+}
+export default NoMatch;
