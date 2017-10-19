fd1a4bd51942f90c660bcff9891432888ecda919
jordanbenzacken <jbzkjbzk@gmail.com>
2017-10-19 08:04:19 +0200

    style and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React, {Component} from 'react';
-import logo from './assets/polygon-harry.jpg';
+import React, { Component } from 'react';
 import SearchView from './views/search-view';
 import Detail from './views/detail';
 import BasketDetail from './views/basket-detail';
 import NoMatch from './views/no-match';
-import Basket from './components/basket';
+import Header from './components/header';
 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import './styles';
 
@@ -16,16 +15,14 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="app-logo" alt="logo"/>
-            <h1 className="App-title">Welcome to Super Potier Store</h1>
-            <Basket/>
+            <Header />
           </header>
           <body className="App-body">
             <Switch>
-              <Route exact path="/" component={SearchView}/>
-              <Route path="/detail/:isbn" component={Detail}/>
-              <Route path="/basket/detail" component={BasketDetail}/>
-              <Route component={NoMatch}/>
+              <Route exact path="/" component={SearchView} />
+              <Route path="/detail/:isbn" component={Detail} />
+              <Route path="/basket/detail" component={BasketDetail} />
+              <Route component={NoMatch} />
             </Switch>
           </body>
         </div>
---
+++ src/components/header/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Basket from '../basket';
+import logo from '../../assets/polygon-harry.jpg';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div data-component='header'>
+                <Link to={"/"}>
+                    <img src={logo} className="app-logo" alt="logo" />
+                </Link>
+                <h1 className="App-title">Welcome to Potier Store</h1>
+                <Basket />
+            </div>
+        )
+    };
+}
+
+export default Header;
