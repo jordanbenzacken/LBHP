3a349987fdf68523f33a560cca9731fee7102fda
jordanbenzacken <jbzkjbzk@gmail.com>
2017-10-19 22:34:14 +0200

    remove some comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import reducers from './redux/reducers';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import thunk from 'redux-thunk';
 import 'material-design-icons/iconfont/material-icons.css'
-
-// See doc react-script. post css ? i18n , devtools
-// TODO : popin detail, add to basket with new view, header, local storage ??
-// material compnents package nicely -- seearch nok (trim, contains)
 
 //
 //get basket after refresh.
@@ -24,6 +20,6 @@ let store = createStore(reducers, {
 
 ReactDOM.render(
     <Provider store={store}>
-    <App/>
-</Provider>, document.getElementById('root'));
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
